2f36dbb22c17fe844a71adff7abe104f0f885025
shayanbalochofficial <144522960+shayanbalochofficial@users.noreply.github.com>
2024-04-15 22:55:26 +0500

    Initial commit

---
+++ conditional.js
@@ -0,0 +1,11 @@
+"use strict";
+//* Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.
+let car = 'subaru';
+for (let i = 0; i < 5; i++) {
+    console.log("Is car == 'subaru'? I predict True.");
+    console.log(car == 'subaru'); // True
+}
+for (let i = 0; i < 5; i++) {
+    console.log("Is car === 'Subaru'? I predict False.");
+    console.log(car === 'Subaru'); // False
+}
